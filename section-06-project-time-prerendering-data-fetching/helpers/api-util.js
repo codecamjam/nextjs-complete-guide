f2d61d9a16026900296144ac06112af9230b789e
codecamjam <camerontaghlabi@gmail.com>
2022-03-20 13:24:01 -0400

    l117: adding ssg on the homepage

---
+++ section-06-project-time-prerendering-data-fetching/helpers/api-util.js
@@ -0,0 +1,40 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://nextjs-course-c067e-default-rtdb.firebaseio.com/events.json'
+  );
+
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
+
+export function getEventById(id) {
+  return DUMMY_EVENTS.find((event) => event.id === id);
+}
