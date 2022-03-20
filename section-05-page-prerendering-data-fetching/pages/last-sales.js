abe6f4443ff3c078934c00ea723220d4c9daf87b
codecamjam <camerontaghlabi@gmail.com>
2022-03-20 12:12:36 -0400

    l111: using the useSWR Nextjs hook

--- section-05-page-prerendering-data-fetching/pages/last-sales.js
+++ section-05-page-prerendering-data-fetching/pages/last-sales.js
@@ -1,34 +1,63 @@
 import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
+//useSWR(<request-url>, (url) => fetch(url).then(res => res.json()))
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://nextjs-course-c067e-default-rtdb.firebaseio.com/sales.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+  const { data, error } = useSWR(
+    'https://nextjs-course-c067e-default-rtdb.firebaseio.com/sales.json',
+    fetcher
+  );
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  async function fetcher(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error();
+    return response.json();
   }
 
-  if (!sales) {
-    return <p>No Data Yet</p>;
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch('https://nextjs-course-c067e-default-rtdb.firebaseio.com/sales.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to Load.</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
