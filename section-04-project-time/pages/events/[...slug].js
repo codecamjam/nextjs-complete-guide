114aa83eebde2e04dd256f5e2ab88477cca32d82
codecamjam <camerontaghlabi@gmail.com>
2022-03-19 22:17:10 -0400

    l82: final steps

--- section-04-project-time/pages/events/[...slug].js
+++ section-04-project-time/pages/events/[...slug].js
@@ -1,5 +1,9 @@
 import { useRouter } from 'next/router';
 import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert/error-alert';
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -27,7 +31,17 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values.</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values.</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -36,12 +50,25 @@ function FilteredEventsPage() {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
+
+  //0 based index so it expects month to begin at 0
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
     </div>
   );
 }
