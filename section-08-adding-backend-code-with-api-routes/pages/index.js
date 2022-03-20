ad96c43b840c204281fb8a8fa182570060198410
codecamjam <camerontaghlabi@gmail.com>
2022-03-20 19:45:01 -0400

    l144: using api routes to get data

--- section-08-adding-backend-code-with-api-routes/pages/index.js
+++ section-08-adding-backend-code-with-api-routes/pages/index.js
@@ -1,10 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
-  function submitFormHandler(event) {
+  async function submitFormHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
@@ -14,16 +15,32 @@ function HomePage() {
       email: enteredEmail,
       text: enteredFeedback,
     };
-
-    //don't need an absolute path
-    //bc this will be appended as an abs path after our domain
-    fetch('/api/feedback', {
+    // fetch('/api/feedback', {
+    //   method: 'POST',
+    //   body: JSON.stringify(reqBody),
+    //   headers: { 'Content-Type': 'application/json' },
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+    const response = await fetch('/api/feedback', {
       method: 'POST',
       body: JSON.stringify(reqBody),
       headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async function loadFeedbackHandler() {
+    // fetch('/api/feedback')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setFeedbackItems(data.feedback);
+    //   });
+
+    const response = await fetch('/api/feedback');
+    const data = await response.json();
+    setFeedbackItems(data.feedback);
   }
 
   return (
@@ -40,6 +57,13 @@ function HomePage() {
         </div>
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>LoadFeedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
