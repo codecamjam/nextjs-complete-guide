d39b8cfd54f80317a134a378f802a54e2730f0e1
codecamjam <camerontaghlabi@gmail.com>
2022-03-21 21:53:23 -0400

    l160: add error handling

--- section-09-project-time-api-routes/pages/api/newsletter.js
+++ section-09-project-time-api-routes/pages/api/newsletter.js
@@ -1,5 +1,21 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+
+  const client = await MongoClient.connect(DB);
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection('newsletter').insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,16 +25,20 @@ async function handler(req, res) {
       return;
     }
 
-    const DB = process.env.DATABASE.replace(
-      '<PASSWORD>',
-      process.env.DATABASE_PASSWORD
-    );
-
-    const client = await MongoClient.connect(DB);
-    const db = client.db();
-    await db.collection('newsletter').insertOne({ email: userEmail });
-
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
 
     res.status(201).json({ message: 'Signed up!' });
   }
