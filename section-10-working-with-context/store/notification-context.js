1b129cfe0aac1117b25be8dd3c6d514412056d2e
codecamjam <camerontaghlabi@gmail.com>
2022-03-22 19:48:30 -0400

    l168: add context state

--- section-10-working-with-context/store/notification-context.js
+++ section-10-working-with-context/store/notification-context.js
@@ -2,16 +2,37 @@ import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null, //{title, message, status}
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+    // setActiveNotification({
+    //   title: notification.title,
+    //   message: notification.message,
+    //   status: notification.status,
+    // });
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
 }
 
-export default notificationContext;
+export default NotificationContext;
