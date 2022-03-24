d3ad960cd7df231c2e2c3ffb7fe8eef188bfec6d
codecamjam <camerontaghlabi@gmail.com>
2022-03-23 21:58:45 -0400

    l184: adding the all posts page

--- section-11-complete-app-blog/pages/posts/index.js
+++ section-11-complete-app-blog/pages/posts/index.js
@@ -1,10 +1,48 @@
 import { Fragment } from 'react';
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes building fullstack React apps easy',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes building fullstack React apps easy',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes building fullstack React apps easy',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes building fullstack React apps easy',
+    date: '2022-02-10',
+  },
+  ,
+];
 
 function AllPostsPage() {
-  return <Fragment>{/* <Hero /> <FeaturedPosts /> */}</Fragment>;
+  return (
+    <Fragment>
+      <AllPosts posts={DUMMY_POSTS} />
+    </Fragment>
+  );
 }
 
 export default AllPostsPage;
-
-// 1) Hero => Present ourselves
-// 2) featured posts
